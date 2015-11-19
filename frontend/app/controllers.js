42b8eceb40491b05966612401f44d02b9cf3635f
Stefan Antoni <stefan@antoni.io>
2015-11-19 22:55:39 +0100

    Articles can be created

--- frontend/app/controllers.js
+++ frontend/app/controllers.js
@@ -7,38 +7,43 @@
 var AppControllers = angular.module('AppControllers', []);
 
 AppControllers.controller('MainCtrl', function ($scope, $location, $http, $window, cfg) {
-
-  console.log("controller MainCtrl called");
-  console.log(cfg);
-
   // Instantiate objects to store your scope data in (a good practice)
   $scope.data = {};
   $scope.functions = {};
 
   // Fetch and display all articles
   $http.get(cfg.listArticlesResource)
+  .then(function(response){
+    $scope.data.articles = response.data;
+  }, function(response){
+    console.log("fail!" + response)
+  });
+
+  // Delete article by ID
+  $scope.functions.deleteArticle = function(articleID) {
+    $http.delete(cfg.articleResource + "/" + articleID)
     .then(function(response){
-      console.log("success" + response);
-      $scope.data.articles = response.data;
+      $window.location.reload(); // TODO: Omit full reload somehow
     }, function(response){
       console.log("fail!" + response)
     });
-
-    // Delete article by ID
-    $scope.functions.deleteArticle = function(articleID) {
-      $http.delete(cfg.articleResource + "/" + articleID)
-        .then(function(response){
-          console.log("success" + response);
-          $window.location.reload(); // TODO: Omit full reload somehow
-        }, function(response){
-          console.log("fail!" + response)
-        });
-    }
+  }
 });
 
 AppControllers.controller('NewArticleCtrl', function($scope, $http, cfg) {
-  console.log("controller NewArticleCtrl called");
+  $scope.functions = {};
+  $scope.functions.createArticle = function(articleID) {
+    $scope.blogpost = {};
 
-  $scope.data = {}
+    $scope.blogpost.title = $scope.title;
+    $scope.blogpost.subtitle = $scope.subtitle;
+    $scope.blogpost.body = $scope.body;
 
+    $http.post(cfg.articleResource + "/create", $scope.blogpost)
+    .then(function(response) {
+      $window.location.reload(); // TODO: Omit full reload somehow
+    }, function(response){
+      console.log("fail!" + response)
+    });
+  }
 });
